44b0425c202daa8b17496fcf407b4885bc020b2a
anastasia <anastasiiadesiateryk@gmail.com>
2025-04-20 23:38:57 +0300

    changed origin for deploy

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
+    origin: "https://memorygamepuzzlegame.netlify.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
